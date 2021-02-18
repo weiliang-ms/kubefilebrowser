0b5a83c1a63f22a5490ac7f03a767b2dd3be37f7
xmapst <xmapst@gmail.com>
2021-02-18 11:27:24 +0800

    change build.sh

--- build.sh
+++ build.sh
@@ -7,23 +7,33 @@ if [ "$?" != "0" ]; then
   exit 1
 fi
 BinaryName="kubecp"
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -installsuffix cgo -ldflags "-s -w" -o $BinaryName .
+version="1.0-bate"
+osList="linux windows darwin"
 # shellcheck disable=SC2181
-if [ "$?" != "0" ]; then
-  echo "!!!!!!Compilation error, please check the source code!!!!!!"
-  exit 1
-fi
+for i in $osList; do
+  # shellcheck disable=SC2027
+  BinaryName=$name"_"$i"-"$version
+  if [ "$i" == "windows" ];then
+    BinaryName=${BinaryName}.exe
+  fi
+  CGO_ENABLED=0 GOOS=$i GOARCH=amd64 go build -a -installsuffix cgo -ldflags "-s -w" -o "$BinaryName" .
+  # shellcheck disable=SC2181
+  if [ "$?" != "0" ]; then
+    echo "!!!!!!Compilation error, please check the source code!!!!!!"
+    exit 1
+  fi
 
-strip --strip-unneeded $BinaryName
-# shellcheck disable=SC2181
-if [ "$?" != "0" ]; then
-  echo "!!!!!!Strip failed!!!!!!"
-  exit 1
-fi
+#  strip --strip-unneeded $BinaryName
+#  # shellcheck disable=SC2181
+#  if [ "$?" != "0" ]; then
+#    echo "!!!!!!Strip failed!!!!!!"
+#    exit 1
+#  fi
 
-upx --lzma $BinaryName
-# shellcheck disable=SC2181
-if [ "$?" != "0" ]; then
-  echo "!!!!!!Upx failed!!!!!!"
-  exit 1
-fi
+  upx --lzma "$BinaryName"
+  # shellcheck disable=SC2181
+  if [ "$?" != "0" ]; then
+    echo "!!!!!!Upx failed!!!!!!"
+    exit 1
+  fi
+done
